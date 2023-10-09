ff75f533ee9050651bd2d99b9a3cce3ec7fe0aab
root309 <choco00101x@gmail.com>
2023-10-10 07:25:42 +0900

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod git_ops;
+mod ui;
+use crate::git_ops::git_functions;
+use std::io::stdout;
+use crossterm::{
+    execute,
+    terminal::{Clear, ClearType},
+    event::{read, KeyEvent, KeyCode, KeyModifiers},
+    cursor::{self, MoveTo},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let branches = git_ops::list_branches();
+    let selected_index = ui::display_palette(&branches);
+    // ユーザーがEnterを押した後に画面をクリア
+    execute!(stdout(), Clear(ClearType::All)).unwrap();
 }
