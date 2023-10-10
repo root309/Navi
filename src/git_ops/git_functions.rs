b354cd4d5294c6781ed0e3216131dc9339dd8eec
root309 <choco00101x@gmail.com>
2023-10-10 09:26:54 +0900

    Add: コメントアウトの追加

--- src/git_ops/git_functions.rs
+++ src/git_ops/git_functions.rs
@@ -1,17 +1,20 @@
 use git2::{Repository, BranchType};
-
+// ブランチリスト
 pub fn list_branches() -> Vec<String> {
     let repo = Repository::open(".").unwrap();
     let branches = repo.branches(Some(BranchType::Local)).unwrap();
     branches.map(|b| b.unwrap().0.name().unwrap().unwrap().to_string()).collect()
 }
+// ブランチチェックアウト
 pub fn checkout_branch(branch_name: &str) -> Result<(), git2::Error> {
     let repo = Repository::open(".")?;
     repo.set_head(&format!("refs/heads/{}", branch_name))?;
     Ok(())
 }
+// ブランチ削除
 pub fn delete_branch(branch_name: &str) {
     let repo = Repository::open(".").unwrap();
-    let mut branch = repo.find_branch(branch_name, BranchType::Local).unwrap();  // `mut`を追加
+    let mut branch = repo.find_branch(branch_name, BranchType::Local).unwrap();
     branch.delete().unwrap();
 }
+// TODO:gitの機能を以下に書く
