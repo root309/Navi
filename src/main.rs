63e8b211562713cf74fcbe3551eea8457415f26d
chocomint9 <choco00101x@gmail.com>
2023-10-20 20:18:07 +0900

    Improve terminal compatibility by enabling raw mode in crossterm

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ use crate::git_ops::git_functions;
 use std::io::stdout;
 use crossterm::{
     execute,
-    terminal::{Clear, ClearType},
+    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode},
     event::{read, KeyEvent, KeyCode, KeyModifiers},
     cursor::{self, MoveTo},
 };
 
 fn main() {
+    enable_raw_mode().unwrap();
     let branches = git_ops::list_branches();
     let selected_index = ui::display_palette(&branches);
     // ユーザーがEnterを押した後に画面をクリア
     execute!(stdout(), Clear(ClearType::All)).unwrap();
+    disable_raw_mode().unwrap();
 }
