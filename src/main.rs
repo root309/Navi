46e31712fd5d1f542c583cf8ea1f4ac0d312e23b
root309 <choco00101x@gmail.com>
2023-11-07 21:37:34 +0900

    fix: typo

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,6 @@ fn main() {
     all_branches.extend(remote_branches.iter().cloned());
     let selected_index = ui::display_palette(&all_branches);
     // ユーザーがEnterを押した後に画面をクリア
-    execute!(stdout(), Clear(ClearType::All)).unwrap(); 
-    enable_raw_mode().unwrap();
-    let branches = git_ops::list_branches();
-    let selected_index = ui::display_palette(&branches);
-    // ユーザーがEnterを押した後に画面をクリア
     execute!(stdout(), Clear(ClearType::All)).unwrap();
     disable_raw_mode().unwrap();
 }
