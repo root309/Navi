581a43412cc2bdf58787ed19fde4abd83ab78fc2
kanzaki582 <89129822+kanzaki582@users.noreply.github.com>
2023-11-11 16:49:47 +0900

    feat: メニューのbash対応 (#9)
    * removed dialoguer
    
    * fix: メインメニューがbashに対応していない問題の修正
    
    * fix: コミットオブジェクトの表示がbashに対応していない問題の修正

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,76 @@
 mod git_ops;
 mod ui;
 use crate::git_ops::git_functions;
-use std::io::stdout;
-use crossterm::{
-    execute,
-    terminal::{Clear, ClearType, enable_raw_mode, disable_raw_mode},
+use crate::git_ops::git_functions::{
+    create_branch_from_commit_interactive, list_branches, list_remote_branches,
 };
-use crate::git_ops::git_functions::{list_branches, list_remote_branches, create_branch_from_commit_interactive};
-use dialoguer::Select;
+use crossterm::{
+    cursor::{MoveTo, Show, Hide},
+    style::Print,
+    execute,
+    event::{read, Event, KeyCode},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+};
+use std::io::{stdout, Write};
 
-fn main() {
-    // ターミナルを初期化
+fn main() -> crossterm::Result<()> {
     enable_raw_mode().unwrap();
 
-    // アクションの選択肢
-    let actions = ["List and select branches", "Create branch from a commit"];
+    let mut stdout = stdout();
+    execute!(stdout, Hide)?; // カーソルを隠す
 
-    // メインメニューを表示
-    let action = Select::new()
-        .with_prompt("Choose an action")
-        .default(0)
-        .items(&actions[..])
-        .interact()
-        .unwrap();
+    let actions = ["List and select branches", "Create branch from a commit"];
+    let mut selected = 0;
+
+    loop {
+        execute!(stdout, MoveTo(0, 0), Clear(ClearType::All))?; // 画面をクリアし、カーソルを左上に移動
+
+        // メニューの表示
+        for (index, action) in actions.iter().enumerate() {
+            execute!(stdout, MoveTo(0, index as u16))?; // カーソルを適切な位置に移動
+            if index == selected {
+                execute!(stdout, Print(format!("> {}\n", action)))?; // Printコマンドを使用
+            } else {
+                execute!(stdout, Print(format!("  {}\n", action)))?; // Printコマンドを使用
+            }
+        }
+        stdout.flush()?;
+
+        // キー入力の処理
+        match read()? {
+            Event::Key(event) => {
+                match event.code {
+                    KeyCode::Char('j') => if selected > 0 { selected -= 1 },
+                    KeyCode::Char('k') => if selected < actions.len() - 1 { selected += 1 },
+                    KeyCode::Enter => break,
+                    _ => (),
+                }
+            }
+            _ => (),
+        }
+    }
 
     // 選択されたアクションに基づいて処理
-    match action {
+    match selected {
         0 => {
             let local_branches = list_branches();
             let remote_branches = list_remote_branches();
             let mut all_branches = local_branches;
             all_branches.extend(remote_branches.iter().cloned());
             ui::display_palette(&all_branches); // ブランチリストを表示する関数を呼び出し
-        },
+        }
         1 => {
             if let Err(e) = git_ops::create_branch_from_commit_interactive() {
                 eprintln!("Error: {}", e);
             }
-        },
+        }
         _ => unreachable!(),
     }
 
-    // ターミナルを終了
-    execute!(stdout(), Clear(ClearType::All)).unwrap();
+    execute!(stdout, Show)?; // カーソルを表示
     disable_raw_mode().unwrap();
+    Ok(())
 }
